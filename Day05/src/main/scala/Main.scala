c642194e92c431b849a3e494b2e869852a6d2959
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-11-05 13:33:24 +0000

    Day 5 part 2 in simplistic manner

--- Day05/src/main/scala/Main.scala
+++ Day05/src/main/scala/Main.scala
@@ -5,10 +5,11 @@ object Main {
   def main(args: Array[String]): Unit = {
     val lines = Source.fromResource("input.txt").getLines.toSeq
     part1(lines)
+    part2(lines)
   }
 
   private def part1(lines: Seq[String]): Unit = {
-    val answer = lines.count(checkString)
+    val answer = lines.count(checkString1)
     println(s"part 1 answer: $answer")
   }
 
@@ -30,9 +31,27 @@ object Main {
   private def hasNoDisallowedValues(s: String): Boolean =
     DisallowedValues.map(s.contains(_)).forall(b => !b)
 
-  private def checkString(s: String): Boolean = {
+  private def checkString1(s: String): Boolean =
     hasMinThreeVowels(s) &&
     hasDoubleLetter(s) &&
     hasNoDisallowedValues(s)
+
+  private def part2(lines: Seq[String]): Unit = {
+    val answer = lines.count(checkString2)
+    println(s"part 1 answer: $answer")
   }
+
+  private def newRule1(s: String): Boolean = {
+    val prs = s.sliding(2).zipWithIndex
+    val bs = prs.map {
+      case (pr, index) => s.indexOf(pr, index + 2) > 0
+    }
+    bs.exists(identity)
+  }
+
+  private def newRule2(s: String): Boolean =
+    s.sliding(3).exists(pr => pr(0) == pr(2))
+
+  private def checkString2(s: String): Boolean =
+    newRule1(s) && newRule2(s)
 }
