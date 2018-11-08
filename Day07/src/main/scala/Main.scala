f034d1e5aaf50360f2ec8679703987ab0db3f2aa
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-11-08 23:10:46 +0000

    Day 7 part 1 working

--- Day07/src/main/scala/Main.scala
+++ Day07/src/main/scala/Main.scala
@@ -1,89 +1,198 @@
-import scala.io.Source
+import scala.concurrent.{Future, Promise}
+import scala.concurrent.ExecutionContext.Implicits.global
+//import scala.io.Source
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-    val lines = Source.fromResource("input.txt").getLines().toList
-    val instructions = parseLines(lines)
+
+//    val lines = Source.fromResource("input.txt").getLines().toList
+//    val instructions = parseLines(lines)
+
+    val instructions = Seq(
+      NotInstruction(Wire("x"), Wire("h")),
+      NotInstruction(Wire("y"), Wire("i")),
+      LeftShiftInstruction(2, Wire("x"), Wire("f")),
+      RightShiftInstruction(2, Wire("y"), Wire("g")),
+      AndInstruction(Wire("x"), Wire("y"), Wire("d")),
+      OrInstruction(Wire("x"), Wire("y"), Wire("e")),
+      ConstantInstruction(Value(123), Wire("x")),
+      ConstantInstruction(Value(456), Wire("y")),
+      PassthroughInstruction(Wire("f"), Wire("p"))
+    )
+
     println(instructions)
     part1(instructions)
   }
 
+  type Signal = Long
+
   sealed abstract class Source
-  final case class Constant(value: Int) extends Source
-  final case class Identifier(name: String) extends Source
+  final case class Wire(name: String) extends Source
+  final case class Value(value: Signal) extends Source
 
-  final case class Wire(name: String)
-
-  sealed abstract class Instruction
-  final case class Load(source: Source, wire: Wire) extends Instruction
-  final case class And(source1: Source, source2: Source, wire: Wire) extends Instruction
-  final case class Or(source1: Source, source2: Source, wire: Wire) extends Instruction
-  final case class Not(source: Source, wire: Wire) extends Instruction
-  final case class LeftShift(source: Source, n: Int, wire: Wire) extends Instruction
-  final case class RightShift(source: Source, n: Int, wire: Wire) extends Instruction
-
-  private def parseLines(lines: Seq[String]): Seq[Instruction] =
-    lines.map(parseLine)
-
-  private final val LoadConstantRegex = """^(\d+) -> ([a-z]+)$""".r
-  private final val LoadIdRegex = """^([a-z]+) -> ([a-z]+)$""".r
-  private final val AndConstantIdRegex = """^(\d+) AND ([a-z]+) -> ([a-z]+)$""".r
-  private final val AndIdIdRegex4 = """^([a-z]+) AND ([a-z]+) -> ([a-z]+)$""".r
-  private final val OrConstantIdRegex = """^(\d+) OR ([a-z]+) -> ([a-z]+)$""".r
-  private final val OrIdIdRegex = """^([a-z]+) OR ([a-z]+) -> ([a-z]+)$""".r
-  private final val NotConstantRegex = """^NOT (\d+) -> ([a-z]+)$""".r
-  private final val NotIdRegex = """^NOT ([a-z]+) -> ([a-z]+)$""".r
-  private final val LeftShiftConstantRegex = """^(\d+) LSHIFT (\d+) -> ([a-z]+)$""".r
-  private final val LeftShiftIdRegex = """^([a-z]+) LSHIFT (\d+) -> ([a-z]+)$""".r
-  private final val RightShiftConstantRegex = """^(\d+) RSHIFT (\d+) -> ([a-z]+)$""".r
-  private final val RightShiftIdRegex = """^([a-z]+) RSHIFT (\d+) -> ([a-z]+)$""".r
-
-  private def parseLine(line: String): Instruction = {
-    line match {
-
-      case LoadConstantRegex(v, w) =>
-        Load(Constant(v.toInt), Wire(w))
-
-      case LoadIdRegex(id, w) =>
-        Load(Identifier(id), Wire(w))
-
-      case AndConstantIdRegex(v, id, w) =>
-        And(Constant(v.toInt), Identifier(id), Wire(w))
-
-      case AndIdIdRegex4(id1, id2, w) =>
-        And(Identifier(id1), Identifier(id2), Wire(w))
-
-      case OrConstantIdRegex(v, id, w) =>
-        Or(Constant(v.toInt), Identifier(id), Wire(w))
-
-      case OrIdIdRegex(id1, id2, w) =>
-        Or(Identifier(id1), Identifier(id2), Wire(w))
-
-      case NotConstantRegex(v, w) =>
-        Not(Constant(v.toInt), Wire(w))
-
-      case NotIdRegex(id, w) =>
-        Not(Identifier(id), Wire(w))
-
-      case LeftShiftConstantRegex(v, n, w) =>
-        LeftShift(Constant(v.toInt), n.toInt, Wire(w))
-
-      case LeftShiftIdRegex(id, n, w) =>
-        LeftShift(Identifier(id), n.toInt, Wire(w))
-
-      case RightShiftConstantRegex(v, n, w) =>
-        RightShift(Constant(v.toInt), n.toInt, Wire(w))
-
-      case RightShiftIdRegex(id, n, w) =>
-        RightShift(Identifier(id), n.toInt, Wire(w))
-
-      case _ => throw new Exception(s"Failed to parse line, '$line'.")
-    }
+  sealed trait Gate {
+    val getOutputValue: Future[Signal]
+  }
+  final case class Constant(value: Value) extends Gate {
+    override val getOutputValue: Future[Signal] = Future.successful(value.value)
+  }
+  final case class Passthrough(input: Future[GetOutputValue]) extends Gate {
+    override val getOutputValue: Future[Signal] = for {
+      f <- input
+      v <- f
+    } yield v
+  }
+  final case class And(input1: Future[GetOutputValue], input2: Future[GetOutputValue]) extends Gate {
+    override val getOutputValue: Future[Signal] = for {
+      f1 <- input1
+      f2 <- input2
+      v1 <- f1
+      v2 <- f2
+    } yield v1 & v2
+  }
+  final case class Or(input1: Future[GetOutputValue], input2: Future[GetOutputValue]) extends Gate {
+    override val getOutputValue: Future[Signal] = for {
+      f1 <- input1
+      f2 <- input2
+      v1 <- f1
+      v2 <- f2
+    } yield v1 | v2
+  }
+  final case class Not(input: Future[GetOutputValue]) extends Gate {
+    override val getOutputValue: Future[Signal] = for {
+      f <- input
+      v <- f
+    } yield ~v & 0xFFFF
+  }
+  final case class LeftShift(by: Int, input: Future[GetOutputValue]) extends Gate {
+    override val getOutputValue: Future[Signal] = for {
+      f <- input
+      v <- f
+    } yield v << by
+  }
+  final case class RightShift(by: Int, input: Future[GetOutputValue]) extends Gate {
+    override val getOutputValue: Future[Signal] = for {
+      f <- input
+      v <- f
+    } yield v >> by
   }
 
+  sealed abstract class Instruction
+  final case class ConstantInstruction(value: Value, output: Wire) extends Instruction
+  final case class PassthroughInstruction(input: Wire, output: Wire) extends Instruction
+  final case class AndInstruction(input1: Wire, input2: Wire, output: Wire) extends Instruction
+  final case class OrInstruction(input1: Wire, input2: Wire, output: Wire) extends Instruction
+  final case class NotInstruction(input: Wire, output: Wire) extends Instruction
+  final case class LeftShiftInstruction(by: Int, input: Wire, output: Wire) extends Instruction
+  final case class RightShiftInstruction(by: Int, input: Wire, output: Wire) extends Instruction
+
+//  private def stringToSource(s: String): Source =
+//    if (s.forall(Character.isDigit)) Value(s.toLong) else Wire(s)
+
+//  private def parseLines(lines: Seq[String]): Seq[Instruction] =
+//    lines.map(parseLine)
+
+//  private final val ConstantRegex = """^(\d+) -> ([a-z]+)$""".r
+//  private final val PassthroughRegex = """^([a-z]+) -> ([a-z]+)$""".r
+//  private final val AndRegex = """^(\d+|[a-z]+) AND (\d+|[a-z]+) -> ([a-z]+)$""".r
+//  private final val OrRegex = """^(\d+|[a-z]+) OR (\d+|[a-z]+) -> ([a-z]+)$""".r
+//  private final val NotRegex = """^NOT (\d+|[a-z]+) -> ([a-z]+)$""".r
+//  private final val LeftShiftRegex = """^(\d+|[a-z]+) LSHIFT (\d+) -> ([a-z]+)$""".r
+//  private final val RightShiftRegex = """^(\d+|[a-z]+) RSHIFT (\d+) -> ([a-z]+)$""".r
+
+//  private def parseLine(line: String): Instruction = {
+//    line match {
+//
+//      case ConstantRegex(s, w) =>
+//        ConstantInstruction(Value(s.toLong), Wire(w))
+//
+//      case PassthroughRegex(s, w) =>
+//        PassthroughInstruction(Wire(s), Wire(w))
+//
+//      case AndRegex(s1, s2, w) =>
+//        AndInstruction(Wire(s1), Wire(s2), Wire(w))
+//
+//      case OrRegex(s1, s2, w) =>
+//        OrInstruction(Wire(s1), Wire(s2), Wire(w))
+//
+//      case NotRegex(s, w) =>
+//        NotInstruction(Wire(s), Wire(w))
+//
+//      case LeftShiftRegex(s, by, w) =>
+//        LeftShiftInstruction(by.toInt, Wire(s), Wire(w))
+//
+//      case RightShiftRegex(s, by, w) =>
+//        RightShiftInstruction(by.toInt, Wire(s), Wire(w))
+//
+//      case _ => throw new Exception(s"Failed to parse line, '$line'.")
+//    }
+//  }
+
+  type GetOutputValue = Future[Signal]
+  type Wires = Map[String, Promise[GetOutputValue]]
+
   private def part1(instructions: Seq[Instruction]): Unit = {
-    val answer = 0
-    println(s"part 1 answer: $answer")
+
+    def op(wires: Wires, instruction: Instruction): Wires = {
+      def lookupWire(wires: Wires, wire: Wire): (Wires, Promise[GetOutputValue]) = {
+        wires.get(wire.name) match {
+          case Some(p) =>
+            (wires, p)
+          case None =>
+            val p = Promise[GetOutputValue]()
+            val newWires = wires.updated(wire.name, p)
+            (newWires, p)
+        }
+      }
+
+      def connect(wires: Wires, output: Wire, gate: Gate): Wires = {
+        val (newWires, promise) = lookupWire(wires, output)
+        promise.success(gate.getOutputValue)
+        newWires
+      }
+
+      instruction match {
+
+        case ConstantInstruction(value, output) =>
+          connect(wires, output, Constant(value))
+
+        case PassthroughInstruction(input, output) =>
+          val (wires2, b) = lookupWire(wires, input)
+          connect(wires2, output, Passthrough(b.future))
+
+        case AndInstruction(input1, input2, output) =>
+          val (wires2, b) = lookupWire(wires, input1)
+          val (wires3, d) = lookupWire(wires2, input2)
+          connect(wires3, output, And(b.future, d.future))
+
+        case OrInstruction(input1, input2, output) =>
+          val (wires2, b) = lookupWire(wires, input1)
+          val (wires3, d) = lookupWire(wires2, input2)
+          connect(wires3, output, Or(b.future, d.future))
+
+        case NotInstruction(input, output) =>
+          val (wires2, b) = lookupWire(wires, input)
+          connect(wires2, output, Not(b.future))
+
+        case LeftShiftInstruction(by, input, output) =>
+          val (wires2, b) = lookupWire(wires, input)
+          connect(wires2, output, LeftShift(by, b.future))
+
+        case RightShiftInstruction(by, input, output) =>
+          val (wires2, b) = lookupWire(wires, input)
+          connect(wires2, output, RightShift(by, b.future))
+      }
+    }
+
+    val initialWires: Wires = Map()
+    val finalWires = instructions.foldLeft(initialWires)(op)
+    val outerFutures = finalWires.values.toList.map(p => p.future)
+    val functionsF = Future.sequence(outerFutures)
+    functionsF.map {
+      innerFutures =>
+        val signalsF = Future.sequence(innerFutures)
+        signalsF.map(signals => println(signals))
+    }
   }
 }
