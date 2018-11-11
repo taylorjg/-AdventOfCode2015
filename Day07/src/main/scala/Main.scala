d62ef9634f4bf98b31313c4e71a491c1c1724956
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-11-11 18:02:39 +0000

    Gates are now just functions of Wires => Wires

--- Day07/src/main/scala/Main.scala
+++ Day07/src/main/scala/Main.scala
@@ -5,33 +5,21 @@ import scala.io.Source
 object Main {
 
   def main(args: Array[String]): Unit = {
-
     val lines = Source.fromResource("input.txt").getLines().toList
-    val instructions = parseLines(lines)
-
-    val part1Answer = run(instructions)
+    val gates = parseLines(lines)
+    val part1Answer = runCircuit(gates)
     println(s"part 1 answer: $part1Answer")
-
-    val part2Answer = run(instructions, Wire("b") -> part1Answer)
+    val part2Answer = runCircuit(gates, Wire("b") -> part1Answer)
     println(s"part 2 answer: $part2Answer")
   }
-
-  type Signal = Long
 
   sealed abstract class Source
   final case class Value(signal: Signal) extends Source
   final case class Wire(name: String) extends Source
 
+  type Signal = Long
   type Wires = Map[Wire, Signal]
-
-  sealed abstract class Instruction
-  final case class Constant(input: Value, output: Wire) extends Instruction
-  final case class Passthrough(input: Wire, output: Wire) extends Instruction
-  final case class Not(input: Source, output: Wire) extends Instruction
-  final case class LeftShift(by: Int, input: Source, output: Wire) extends Instruction
-  final case class RightShift(by: Int, input: Source, output: Wire) extends Instruction
-  final case class And(input1: Source, input2: Wire, output: Wire) extends Instruction
-  final case class Or(input1: Source, input2: Source, output: Wire) extends Instruction
+  type Gate = Wires => Wires
 
   private final val NumberRegex = """(\d+)"""
   private final val WireRegex = """([a-z]+)"""
@@ -44,37 +32,29 @@ object Main {
   private final val AndRegex = s"$SourceRegex AND $SourceRegex -> $WireRegex".r
   private final val OrRegex = s"$SourceRegex OR $SourceRegex -> $WireRegex".r
 
-  private def parseLines(lines: Seq[String]): Seq[Instruction] =
+  private def parseLines(lines: Seq[String]): Seq[Gate] =
     lines.map(parseLine)
 
-  private def parseLine(line: String): Instruction =
+  private def parseLine(line: String): Gate =
     line match {
-
       case ConstantRegex(s, w) =>
-        Constant(Value(s.toLong), Wire(w))
-
+        zeroInputGate(Value(s.toLong), Wire(w))
       case PassthroughRegex(s, w) =>
-        Passthrough(Wire(s), Wire(w))
-
+        oneInputGate(Wire(s), Wire(w), identity)
       case NotRegex(s, w) =>
-        Not(Wire(s), Wire(w))
-
+        oneInputGate(toSource(s), Wire(w), v => ~v & 0xFFFF)
       case LeftShiftRegex(s, by, w) =>
-        LeftShift(by.toInt, Wire(s), Wire(w))
-
+        oneInputGate(toSource(s), Wire(w), v => v << by.toInt)
       case RightShiftRegex(s, by, w) =>
-        RightShift(by.toInt, Wire(s), Wire(w))
-
+        oneInputGate(toSource(s), Wire(w), v => v >> by.toInt)
       case AndRegex(s1, s2, w) =>
-        And(stringToSource(s1), Wire(s2), Wire(w))
-
+        twoInputGate(toSource(s1), toSource(s2), Wire(w), (v1, v2) => v1 & v2)
       case OrRegex(s1, s2, w) =>
-        Or(Wire(s1), Wire(s2), Wire(w))
-
+        twoInputGate(toSource(s1), toSource(s2), Wire(w), (v1, v2) => v1 | v2)
       case _ => throw new Exception(s"Failed to parse line, '$line'.")
     }
 
-  private def stringToSource(s: String): Source =
+  private def toSource(s: String): Source =
     if (s.forall(Character.isDigit)) Value(s.toLong) else Wire(s)
 
   private def setOutput(wires: Wires,
@@ -91,52 +71,27 @@ object Main {
       case wire: Wire   => wires.get(wire)
     }
 
-  private def zeroInputGate(wires: Wires, value: Value, output: Wire): Wires =
+  private def zeroInputGate(value: Value, output: Wire)(wires: Wires): Wires =
     setOutput(wires, output, value.signal.some)
 
-  private def oneInputGate(wires: Wires,
-                           input: Source,
-                           output: Wire,
-                           eval: Signal => Signal): Wires =
+  private def oneInputGate(input: Source, output: Wire, eval: Signal => Signal)(
+      wires: Wires): Wires =
     setOutput(wires, output, tryGetValue(wires, input).map(eval))
 
-  private def twoInputGate(wires: Wires,
-                           input1: Source,
-                           input2: Source,
-                           output: Wire,
-                           eval: (Signal, Signal) => Signal): Wires =
-    setOutput(
-      wires,
-      output,
-      (tryGetValue(wires, input1) |@| tryGetValue(wires, input2))(eval))
+  private def twoInputGate(
+      input1: Source,
+      input2: Source,
+      output: Wire,
+      eval: (Signal, Signal) => Signal)(wires: Wires): Wires =
+    setOutput(wires,
+              output,
+              (tryGetValue(wires, input1) |@| tryGetValue(wires, input2))(eval))
 
-  private def processInstruction(wires: Wires,
-                                 instruction: Instruction): Wires =
-    instruction match {
-      case Constant(input, output) =>
-        zeroInputGate(wires, input, output)
-      case Passthrough(input, output) =>
-        oneInputGate(wires, input, output, identity)
-      case Not(input, output) =>
-        oneInputGate(wires, input, output, v => ~v & 0xFFFF)
-      case LeftShift(by, input, output) =>
-        oneInputGate(wires, input, output, v => v << by)
-      case RightShift(by, input, output) =>
-        oneInputGate(wires, input, output, v => v >> by)
-      case And(input1, input2, output) =>
-        twoInputGate(wires, input1, input2, output, (v1, v2) => v1 & v2)
-      case Or(input1, input2, output) =>
-        twoInputGate(wires, input1, input2, output, (v1, v2) => v1 | v2)
-    }
+  private def evalGates(initialWires: Wires, gates: Seq[Gate]): Wires =
+    gates.foldLeft(initialWires)((wires, gate) => gate(wires))
 
-  private def processInstructions(wires: Wires,
-                                  instructions: Seq[Instruction]): Wires =
-    instructions.foldLeft(wires)(processInstruction)
-
-  private def run(instructions: Seq[Instruction],
-                  overrides: (Wire, Signal)*): Signal = {
-    def loop(wires: Wires): Signal =
-      wires.getOrElse(Wire("a"), loop(processInstructions(wires, instructions)))
+  private def runCircuit(gates: Seq[Gate], overrides: (Wire, Signal)*): Signal = {
+    def loop(wires: Wires): Signal = wires.getOrElse(Wire("a"), loop(evalGates(wires, gates)))
     val initialWires: Wires = Map(overrides: _*)
     loop(initialWires)
   }
