b1b2ae98361c5b632428fef999fa0b7c5939ea31
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-11-18 10:51:05 +0000

    Blah day 10

--- Day10/src/main/scala/Main.scala
+++ Day10/src/main/scala/Main.scala
@@ -4,101 +4,32 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     val input = "1321131112"
-    // val part1Answer = part1(input)
-    // println(s"part 1 answer: $part1Answer")
-    // val part2Answer = part2(input)
+    val part1Answer = part1(input)
+    println(s"part 1 answer: $part1Answer")
     val part2Answer = part2(input)
     println(s"part 2 answer: $part2Answer")
   }
 
   private def part1(input: String): Int =
-    List.iterate(input, 41)(generateNextString).last.length
+    Stream.iterate(input, 41)(generateNextString).last.length
+
+  private def part2(input: String): Int =
+    Stream.iterate(input, 51)(generateNextString).last.length
 
   private def generateNextString(s: String): String = {
     @tailrec
-    def loop(remaining: String, acc: String): String = {
+    def loop(remaining: String, acc: List[String]): List[String] = {
       if (remaining.isEmpty) acc
       else {
         val ch = remaining.head
-        val run = remaining.takeWhile(_ == ch)
-        val count = run.length
-        val sequence = s"$count$ch"
-        loop(remaining.substring(count), acc + sequence)
+        val (run, remaining2) = remaining.span(_ == ch)
+        val say = s"${run.length}$ch"
+        loop(remaining2, say :: acc)
       }
     }
-    loop(s, "")
+    val says = loop(s, List()).reverse
+    val sb = new StringBuilder
+    says.foldLeft(sb)((sb, s) => sb ++= s)
+    sb.toString
   }
-
-  private def part2(input: String): Int = {
-    val initialPairs = toPairs(input)
-    val finalPairs = List.iterate(initialPairs, 41)(generateNextPairs).last
-    // val finalPairs = List.iterate(initialPairs, 7)(generateNextPairs).last
-    pairsLength(finalPairs)
-  }
-
-  type Count = Int
-  type Digit = Char
-  type Pair = (Count, Char)
-  type Pairs = List[Pair]
-
-  private def toPairs(input: String): Pairs = {
-    @tailrec
-    def loop(remaining: String, acc: Pairs): Pairs = {
-      if (remaining.isEmpty) acc
-      else {
-        val ch = remaining.head
-        val run = remaining.takeWhile(_ == ch)
-        val count = run.length
-        val pair = (count, ch)
-        loop(remaining.substring(count), pair :: acc)
-      }
-    }
-    loop(input, List()).reverse
-  }
-
-  // 1 becomes 1-1
-  // [(1, 1)] => [(2, 1)]
-
-  // 1-1 becomes 2-1
-  // [(2, 1)] => [(1, 2), (1, 1)]
-
-  // 2-1 becomes 12-11
-  // [(1, 2), (1, 1)] => [(1,1), (1,2), (2,1)]
-
-  // 12-11 becomes 11-12-21
-  // [(1,1), (1,2), (2,1)] => [(3,1), (2,2), (1,1)]
-  // [(1,1), (1,2)] "12" => "1112" => [(3,1), (1,2)]
-  // [(1,2), (2,1)] "211" => "1221" => [(1,1), (2,2), (1,1)]
-
-  // 11-12-21 becomes 31-22-11
-  // [(3,1), (2,2), (1,1)] => [(1,3), (1,1), (2,2), (2,1)]
-
-  // ----------------------------------------
-
-  // 1 => [(1, 1)]
-  // [(1, 1)] => [(2, 1)]
-  // [(2, 1)] => [(1, 2), (1, 1)]
-  // [(1, 2), (1, 1)] => [(1,1), (1,2), (2,1)]
-  // [(1,1), (1,2), (2,1)] => [(3,1), (2,2), (1,1)]
-  // [(3,1), (2,2), (1,1)] => [(1,3), (1,1), (2,2), (2,1)]
-
-  private def generateNextPairs(pairs: Pairs): Pairs = {
-    // println(s"[generateNextPairs] $pairs")
-    pairs.foldLeft(List[Pair]()) {
-      case (acc, pair) =>
-        val nextString = generateNextString(pair._2.toString * pair._1)
-        val nextPairs = toPairs(nextString)
-        val firstNextPair = nextPairs.head
-        acc.lastOption match {
-          case Some(lastPair) if lastPair._2 == firstNextPair._2 =>
-            val combinedPair = (lastPair._1 + firstNextPair._1, lastPair._2)
-            acc.init ++ List(combinedPair) ++ nextPairs.tail
-          case _ =>
-            acc ++ nextPairs
-        }
-    }
-  }
-
-  private def pairsLength(pairs: Pairs): Int =
-    pairs.map(_._1).sum
 }
