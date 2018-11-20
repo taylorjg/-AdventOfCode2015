db200639e15bfb5b45442424b69160ce9912e079
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-11-20 21:35:16 +0000

    Day12 part 2

--- Day12/src/main/scala/Main.scala
+++ Day12/src/main/scala/Main.scala
@@ -9,6 +9,8 @@ object Main {
     val jvalue = parse(input)
     val part1Answer = part1(jvalue)
     println(s"Part 1 answer: $part1Answer")
+    val part2Answer = part2(jvalue)
+    println(s"Part 2 answer: $part2Answer")
   }
 
   private def part1(jvalue: JValue): Int = {
@@ -18,6 +20,18 @@ object Main {
         case JArray(jvs)    => acc + jvs.foldLeft(0)(loop)
         case JObject(pairs) => acc + pairs.map(_._2).foldLeft(0)(loop)
         case _              => acc
+      }
+    loop(0, jvalue)
+  }
+
+  private def part2(jvalue: JValue): Int = {
+    def loop(acc: Int, jv: JValue): Int =
+      jv match {
+        case JInt(n)                                                => acc + n.toInt
+        case JArray(jvs)                                            => acc + jvs.foldLeft(0)(loop)
+        case JObject(pairs) if pairs.exists(_._2 == JString("red")) => acc
+        case JObject(pairs)                                         => acc + pairs.map(_._2).foldLeft(0)(loop)
+        case _                                                      => acc
       }
     loop(0, jvalue)
   }
