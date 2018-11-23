8ad7869014a3a748f60b051c10b1efa667ab909b
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-11-23 20:08:07 +0000

    Day 13 part 1

--- Day13/src/main/scala/Main.scala
+++ Day13/src/main/scala/Main.scala
@@ -4,29 +4,59 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     val lines = Source.fromResource("input.txt").getLines().toList
-    val placements = parseLines(lines)
-    println(placements)
-    val part1Answer = part1(placements)
+    // val lines = Source.fromResource("test.txt").getLines().toList
+    val pairings = parseLines(lines)
+    val part1Answer = part1(pairings)
     println(s"part 1 answer: $part1Answer")
   }
 
-  final case class Placement(person1: String,
-                             person2: String,
-                             happinessDelta: Int)
+  final case class Line(person1: String, person2: String, delta: Int)
 
-  private final val PlacementRegex =
+  final case class Pairing(person1: String,
+                           person2: String,
+                           gain: Int,
+                           loss: Int)
+
+  private final val LineRegex =
     raw"""(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+)\.""".r
 
-  private def parseLines(lines: Seq[String]): Seq[Placement] =
-    lines.map(parseLine)
+  private def parseLines(strings: Seq[String]): Seq[Pairing] = {
+    val lines = strings.map(parseLine)
+    val grouped = lines.groupBy { line =>
+      val names = List(line.person1, line.person2).sorted
+      s"${names.head}:${names.last}"
+    }
+    grouped.values
+      .map(v =>
+        Pairing(v.head.person1, v.head.person2, v.head.delta, v.last.delta))
+      .toSeq
+  }
 
-  private def parseLine(line: String): Placement =
+  private def parseLine(line: String): Line =
     line match {
-      case PlacementRegex(p1, "gain", dh, p2) => Placement(p1, p2, +dh.toInt)
-      case PlacementRegex(p1, "lose", dh, p2) => Placement(p1, p2, -dh.toInt)
-      case _                                  => throw new Exception(s"Failed to parse line, '$line'.")
+      case LineRegex(p1, "gain", dh, p2) => Line(p1, p2, +dh.toInt)
+      case LineRegex(p1, "lose", dh, p2) => Line(p1, p2, -dh.toInt)
+      case _                             => throw new Exception(s"Failed to parse line, '$line'.")
     }
 
-  private def part1(placements: Seq[Placement]): Int =
-    0
+  private def totalHappinessChange(arrangement: Seq[Pairing]): Int =
+    arrangement.map(p => p.gain + p.loss).sum
+
+  private def distinctPeople(pairings: Seq[Pairing]): Int =
+    pairings.flatMap(p => List(p.person1, p.person2)).distinct.length
+
+  private def allPeopleMentionedTwice(pairings: Seq[Pairing]): Boolean = {
+    val allPersons = pairings.flatMap(p => List(p.person1, p.person2))
+    val distinctPersons = allPersons.distinct
+    distinctPersons.forall(p => allPersons.count(_ == p) == 2)
+  }
+
+  private def part1(pairings: Seq[Pairing]): Int = {
+    val numPeople = distinctPeople(pairings)
+    pairings
+      .combinations(numPeople)
+      .filter(allPeopleMentionedTwice)
+      .map(totalHappinessChange)
+      .max
+  }
 }
