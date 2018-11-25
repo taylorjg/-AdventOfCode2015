a0a8fbde34d7022cb4e6ac20e79a078451f5026f
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-11-25 11:27:24 +0000

    Day 14 part 2

--- Day14/src/main/scala/Main.scala
+++ Day14/src/main/scala/Main.scala
@@ -6,9 +6,12 @@ object Main {
     val lines = Source.fromResource("input.txt").getLines().toList
     // val lines = Source.fromResource("test.txt").getLines().toList
     val deerData = parseLines(lines)
-    // val part1Answer = part1(deerData, 1000)
-    val part1Answer = part1(deerData, 2503)
+    // val numSeconds = 1000
+    val numSeconds = 2503
+    val part1Answer = part1(deerData, numSeconds)
     println(s"part 1 answer: $part1Answer")
+    val part2Answer = part2(deerData, numSeconds)
+    println(s"part 2 answer: $part2Answer")
   }
 
   final case class DeerData(name: String,
@@ -30,11 +33,12 @@ object Main {
     }
 
   private def part1(deerData: Seq[DeerData], numSeconds: Int): Int = {
-    val initialStates = deerData.map(dd => flying(dd, dd.flyingTime, 0))
-    val v1 = Iterable.iterate(initialStates, numSeconds + 1)(advanceStates).toList
-    val v2 = v1.last
-    val v3 = v2.maxBy(s => s.td)
-    v3.td
+    val initialStates = deerData.map(dd => flying(dd, dd.flyingTime, 0, 0))
+    Iterable
+      .iterate(initialStates, numSeconds + 1)(advanceStates)
+      .last
+      .maxBy(_.td)
+      .td
   }
 
   private def advanceStates(states: Seq[State]): Seq[State] =
@@ -42,20 +46,54 @@ object Main {
 
   private def advanceState(state: State): State =
     state match {
-      case Flying(dd, r, td) =>
-        if (r == 1) resting(dd, dd.restingTime, td + dd.speed)
-        else flying(dd, r - 1, td + dd.speed)
-      case Resting(dd, r, td) =>
-        if (r == 1) flying(dd, dd.flyingTime, td)
-        else resting(dd, r - 1, td)
+      case Flying(dd, r, td, pts) =>
+        if (r == 1) resting(dd, dd.restingTime, td + dd.speed, pts)
+        else flying(dd, r - 1, td + dd.speed, pts)
+      case Resting(dd, r, td, pts) =>
+        if (r == 1) flying(dd, dd.flyingTime, td, pts)
+        else resting(dd, r - 1, td, pts)
     }
 
-  sealed abstract class State(dd: DeerData, r: Int, val td: Int)
-  final case class Flying(dd: DeerData, r: Int, override val td: Int)
-      extends State(dd, r, td)
-  final case class Resting(dd: DeerData, r: Int, override val td: Int)
-      extends State(dd, r, td)
+  sealed abstract class State(dd: DeerData, r: Int, val td: Int, val pts: Int)
+  final case class Flying(dd: DeerData,
+                          r: Int,
+                          override val td: Int,
+                          override val pts: Int)
+      extends State(dd, r, td, pts)
+  final case class Resting(dd: DeerData,
+                           r: Int,
+                           override val td: Int,
+                           override val pts: Int)
+      extends State(dd, r, td, pts)
 
-  private def flying(dd: DeerData, r: Int, td: Int): State = Flying(dd, r, td)
-  private def resting(dd: DeerData, r: Int, td: Int): State = Resting(dd, r, td)
+  private def flying(dd: DeerData, r: Int, td: Int, pts: Int): State =
+    Flying(dd, r, td, pts)
+  private def resting(dd: DeerData, r: Int, td: Int, pts: Int): State =
+    Resting(dd, r, td, pts)
+
+  private def part2(deerData: Seq[DeerData], numSeconds: Int): Int = {
+    val initialStates = deerData.map(dd => flying(dd, dd.flyingTime, 0, 0))
+    Iterable
+      .iterate(initialStates, numSeconds + 1)(advanceStates2)
+      .last
+      .maxBy(_.pts)
+      .pts
+  }
+
+  private def advanceStates2(states: Seq[State]): Seq[State] = {
+    val states2 = states.map(advanceState)
+    val tdMax = states2.maxBy(_.td).td
+    states2.map(incrementLeaders(tdMax))
+  }
+
+  private def incrementLeaders(tdMax: Int)(state: State): State = {
+    def addPointIfLeader(td: Int, pts: Int): Int =
+      if (td == tdMax) pts + 1 else pts
+    state match {
+      case Flying(dd, r, td, pts) =>
+        flying(dd, r, td, addPointIfLeader(td, pts))
+      case Resting(dd, r, td, pts) =>
+        resting(dd, r, td, addPointIfLeader(td, pts))
+    }
+  }
 }
