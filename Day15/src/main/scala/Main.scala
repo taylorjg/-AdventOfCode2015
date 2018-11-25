25a5bef765b513eaab2b344bc6d97302a05ede54
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-11-25 19:10:32 +0000

    Day 15 part 2

--- Day15/src/main/scala/Main.scala
+++ Day15/src/main/scala/Main.scala
@@ -4,11 +4,11 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     val lines = Source.fromResource("input.txt").getLines().toList
-    // val lines = Source.fromResource("test.txt").getLines().toList
     val ingredients = parseLines(lines)
-    // val part1Answer = part1a(ingredients)
-    val part1Answer = part1b(ingredients)
+    val part1Answer = part1(ingredients)
     println(s"part 1 answer: $part1Answer")
+    val part2Answer = part2(ingredients)
+    println(s"part 2 answer: $part2Answer")
   }
 
   private final val LineRegex =
@@ -36,20 +36,7 @@ object Main {
       case _ => throw new Exception(s"Failed to parse line, '$line'.")
     }
 
-  private def part1a(ingredients: Seq[Ingredient]): Int = {
-    val scores = for {
-      amount1 <- 0 to 100
-      amount2 <- 0 to 100
-      if amount1 + amount2 == 100
-    } yield
-      calculateScore(
-        List(
-          amount1 -> ingredients(0),
-          amount2 -> ingredients(1)))
-    scores.max
-  }
-
-  private def part1b(ingredients: Seq[Ingredient]): Int = {
+  private def part1(ingredients: Seq[Ingredient]): Int = {
     val scores = for {
       amount1 <- 0 to 100
       amount2 <- 0 to 100
@@ -57,16 +44,32 @@ object Main {
       amount4 <- 0 to 100
       if amount1 + amount2 + amount3 + amount4 == 100
     } yield
-      calculateScore(
-        List(
-          amount1 -> ingredients(0),
-          amount2 -> ingredients(1),
-          amount3 -> ingredients(2),
-          amount4 -> ingredients(3)))
+      calculateScore(List(amount1 -> ingredients(0),
+                          amount2 -> ingredients(1),
+                          amount3 -> ingredients(2),
+                          amount4 -> ingredients(3)),
+                     None)
     scores.max
   }
 
-  private def calculateScore(pairs: List[(Int, Ingredient)]): Int = {
+  private def part2(ingredients: Seq[Ingredient]): Int = {
+    val scores = for {
+      amount1 <- 0 to 100
+      amount2 <- 0 to 100
+      amount3 <- 0 to 100
+      amount4 <- 0 to 100
+      if amount1 + amount2 + amount3 + amount4 == 100
+    } yield
+      calculateScore(List(amount1 -> ingredients(0),
+                          amount2 -> ingredients(1),
+                          amount3 -> ingredients(2),
+                          amount4 -> ingredients(3)),
+                     Some(500))
+    scores.max
+  }
+
+  private def calculateScore(pairs: List[(Int, Ingredient)],
+                             maybeTargetCalories: Option[Int]): Int = {
     def calculatePropertyScore(property: Ingredient => Int): Int = {
       val propertyScores = pairs.map {
         case (amount, ingredient) => amount * property(ingredient)
@@ -77,6 +80,12 @@ object Main {
     val d = calculatePropertyScore(_.durability)
     val f = calculatePropertyScore(_.flavor)
     val t = calculatePropertyScore(_.texture)
-    c * d * f * t
+    val score = c * d * f * t
+    maybeTargetCalories match {
+      case Some(target) =>
+        val calories = calculatePropertyScore(_.calories)
+        if (calories == target) score else 0
+      case None => score
+    }
   }
 }
