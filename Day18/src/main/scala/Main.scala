4aa12bd341c71b7db4e69b1764dee501def100ec
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-11-26 20:12:30 +0000

    Day 18 part 1

--- Day18/src/main/scala/Main.scala
+++ Day18/src/main/scala/Main.scala
@@ -3,8 +3,10 @@ import scala.io.Source
 object Main {
 
   def main(args: Array[String]): Unit = {
-    val fileName = "test.txt" // "input.txt"
-    val numSteps = 4 // 100
+    // val fileName = "test.txt"
+    // val numSteps = 4
+    val fileName = "input.txt"
+    val numSteps = 100
     val lines = Source.fromResource(fileName).getLines().toList
     val grid = parseLines(lines)
     val part1Answer = part1(grid, numSteps)
@@ -21,16 +23,44 @@ object Main {
     line.toVector.map(_ == '#')
 
   private def part1(initialGrid: Grid, numSteps: Int): Int = {
-    initialGrid.foreach(println)
     val finalGrid = Iterable.iterate(initialGrid, numSteps + 1)(step).last
     countOnLights(finalGrid)
   }
 
   private def step(grid: Grid): Grid = {
-    // TODO: implement this properly
-    grid
+    val numRows = grid.length
+    val numCols = grid.head.length
+    Vector.tabulate(numRows, numCols)(newState(grid))
+  }
+
+  private def newState(grid: Grid)(row: Int, col: Int): Boolean = {
+    val isOn = grid(row)(col)
+    val numOnNeighbours = countOnNeighbours(grid, row, col)
+    numOnNeighbours match {
+      case 2 if isOn  => true
+      case 3 if isOn  => true
+      case _ if isOn  => false
+      case 3 if !isOn => true
+      case _          => false
+    }
+  }
+
+  private def countOnNeighbours(grid: Grid, row: Int, col: Int): Int = {
+    val maxRowBound = grid.indices.last
+    val maxColBound = grid.head.indices.last
+    val coords = for {
+      r <- row - 1 to row + 1
+      c <- col - 1 to col + 1
+      if r != row || c != col
+      if r >= 0 && r <= maxRowBound && c >= 0 && c <= maxColBound
+    } yield (r, c)
+    val values = coords.map { case (r, c) => grid(r)(c) }
+    values.count(identity)
   }
 
   private def countOnLights(grid: Grid): Int =
     grid.map(gridRow => gridRow.count(identity)).sum
+
+  // private def dumpGrid(grid: Grid): Unit =
+  //   grid.foreach(gridRow => println(gridRow.map(if (_) '#' else '.').mkString))
 }
