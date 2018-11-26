3c73a9be8fd031800cc29f8465fe7d0a0671e23b
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-11-26 00:28:00 +0000

    Day 16 part 1

--- Day16/src/main/scala/Main.scala
+++ Day16/src/main/scala/Main.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 import scala.io.Source
 
 object Main {
@@ -5,17 +6,31 @@ object Main {
   def main(args: Array[String]): Unit = {
     val lines = Source.fromResource("input.txt").getLines().toList
     val aunts = parseLines(lines)
-    val part1Answer = part1(aunts)
+    val part1Answer = findBestMatch(aunts)
     println(s"part 1 answer: $part1Answer")
   }
+
+  private final val Analysis = List(
+    "children" -> 3,
+    "cats" -> 7,
+    "samoyeds" -> 2,
+    "pomeranians" -> 3,
+    "akitas" -> 0,
+    "vizslas" -> 0,
+    "goldfish" -> 5,
+    "trees" -> 3,
+    "cars" -> 2,
+    "perfumes" -> 1
+  )
 
   private final val LineRegex =
     raw"""Sue (\d+): (\w+): (\d+), (\w+): (\d+), (\w+): (\d+)""".r
 
   type Item = (String, Int)
+  type Items = List[Item]
   final case class Aunt(number: Int, items: List[Item])
 
-  private def parseLines(lines: Seq[String]): Seq[Aunt] =
+  private def parseLines(lines: List[String]): List[Aunt] =
     lines.map(parseLine)
 
   private def parseLine(line: String): Aunt =
@@ -25,8 +40,22 @@ object Main {
       case _ => throw new Exception(s"Failed to parse line, '$line'.")
     }
 
-  private def part1(aunts: Seq[Aunt]): Int = {
-    aunts.foreach(println)
-    0
+  private def findBestMatch(aunts: List[Main.Aunt]): Int = {
+    @tailrec
+    def loop(remainingAunts: List[Aunt]): Int = {
+      if (remainingAunts.length == 1) remainingAunts.head.number
+      else {
+        def filterAunts(acc: List[Aunt], item: Item): List[Aunt] =
+          acc.filter { aunt =>
+            aunt.items.find(_._1 == item._1) match {
+              case Some(auntItem) => auntItem._2 == item._2
+              case None           => true
+            }
+          }
+        val remainingAunts2 = Analysis.foldLeft(remainingAunts)(filterAunts)
+        loop(remainingAunts2)
+      }
+    }
+    loop(aunts)
   }
 }
