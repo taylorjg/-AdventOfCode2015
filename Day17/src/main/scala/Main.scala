2ae889adbb84551581806d210ef95a4dc6bf4ae5
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-11-26 13:32:22 +0000

    Day 17 part 2

--- Day17/src/main/scala/Main.scala
+++ Day17/src/main/scala/Main.scala
@@ -7,6 +7,8 @@ object Main {
     val capacities = lines.map(_.toInt)
     val part1Answer = part1(capacities)
     println(s"part 1 answer: $part1Answer")
+    val part2Answer = part2(capacities)
+    println(s"part 2 answer: $part2Answer")
   }
 
   private def part1(capacities: List[Int]): Int = {
@@ -15,5 +17,14 @@ object Main {
     val allCombinations = allCombinationsWithIndices.map(xs => xs.map(_._1))
     val validCombinations = allCombinations.filter(_.sum == 150)
     validCombinations.length
+  }
+
+  private def part2(capacities: List[Int]): Int = {
+    val capacitiesWithIndices = capacities.zipWithIndex
+    val allCombinationsWithIndices = (1 to capacitiesWithIndices.length).flatMap(capacitiesWithIndices.combinations)
+    val allCombinations = allCombinationsWithIndices.map(xs => xs.map(_._1))
+    val validCombinations = allCombinations.filter(_.sum == 150)
+    val minContainers = validCombinations.minBy(_.length).length
+    validCombinations.count(_.length == minContainers)
   }
 }
